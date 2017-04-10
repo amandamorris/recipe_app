8c341174bb617d01498540436fa7fea2c8bfe5fb
Amanda Morris <amandafmorris@gmail.com>
2017-04-10 01:03:18 +0000

    fixed port info for heroku

--- server.py
+++ server.py
@@ -311,4 +311,8 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
 
-    app.run(port=5000, host='0.0.0.0')
+    # app.run(port=5000, host='0.0.0.0')
+
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=PORT)
